89718f011654263cc596fc069b13c38c1c0767ab
Eric Heltemes <e.heltemes83@gmail.com>
2021-04-21 21:33:02 -0500

    users can now delete posts

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -6,12 +6,12 @@ const deletePost = async (event) => {
   if (event.target.hasAttribute('data-id')) {
     const id = event.target.getAttribute('data-id');
 
-    const response = await fetch(`/api/projects/${id}`, {
+    const response = await fetch(`/api/posts/${id}`, {
       method: 'DELETE',
     });
 
     if (response.ok) {
-      document.location.replace('/profile');
+      document.location.replace('/dashboard');
     } else {
       alert('Failed to delete project');
     }
